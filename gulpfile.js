f2b710c81f44600feb06127d57337eb213f37ec3
Samuel <astraloverflow@firemail.cc>
2018-01-16 22:09:21 -0800

    build(gulpfile.js): simplified gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,18 @@
 const gulp = require('gulp')
 const browserSync = require('browser-sync')
-const sourcemaps = require('gulp-sourcemaps')
-const postcss = require('gulp-postcss')
-const postcssReporter = require('postcss-reporter')
-const cssimport = require('postcss-import')
-const cssnext = require('postcss-cssnext')
 
-gulp.task('default', () => gulp.src('./docs/demo/demo.css')
-    .pipe(sourcemaps.init())
-    .pipe(postcss([
-      cssimport(),
-      cssnext(),
-      postcssReporter()
-    ]))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./docs/demo/styles/'))
-)
-
-gulp.task('serve', () => {
-  gulp.watch(['*.css'], ['default'])
+gulp.task('default', () => {
+  gulp.watch(['*.css']).on('change', browserSync.stream)
+  gulp.watch('*.html').on('change', browserSync.reload)
 
   return browserSync({
     // Options found here: https://browsersync.io/docs/options
     open: 'ui',
     reloadDelay: 2000,
     logPrefix: 'BrowserSync',
+    startPath: 'demo/index.html',
     server: {
-      baseDir: './docs/demo/',
+      baseDir: './',
       directory: false
     }
   })
